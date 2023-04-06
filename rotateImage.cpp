f5eef396ffcbbe5db413c39f5d2f87d96fb6eefd
sanket <devrajkumar1948@gmail.com>
2023-04-06 23:57:56 +0530

    q 39

---
+++ rotateImage.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+void rotate(vector<vector<int>> &A)
+{
+    int n = A.size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            swap(A[i][j], A[j][i]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        reverse(A[i].begin(), A[i].end());
+    }
+}
+int main()
+{
+    return 0;
+}
